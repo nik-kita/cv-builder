11cc6827b2cbbee8cbabf9420ebd156c7a5f19c0
nik-kita <nik-kita.deno.dev>
2024-04-28 10:22:49 +0200

    eslint semi, vue3-google-login

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import "./assets/main.css";
-
+import Vue3GoogleLogin from 'vue3-google-login';
 import { createApp } from "vue";
 import App from "./App.vue";
 import { init_router } from "./router";
 
+
 const app = createApp(App);
 
 app.use(init_router());
+app.use(Vue3GoogleLogin, {
+  clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+});
 
 app.mount("#app");
