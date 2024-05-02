1b4c6c15011468371e2046ae6df640ed13d5e3b4
nik-kita <nik-kita.deno.dev>
2024-05-02 06:32:29 +0200

    Update tokens.ts and types.d.ts files

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,5 @@
+import type { paths } from "./api/openapi";
+
 declare global {
   type Tail<T extends any[]> = T extends [infer _first, ...infer Rest] ? Rest : never;
   type OmitStrict<
@@ -13,6 +15,11 @@ declare global {
     T extends Record<string, any>,
     U extends Record<string, any>
   > = Omit<T, keyof U> & U;
+
+  type Method = 'get' | 'post' | 'put' | 'delete' | 'patch';
+
+  type ApiReq<M extends Method, T extends keyof paths> = paths[T][M]['requestBody']['content']['application/json'];
+  type ApiRes<M extends Method, T extends keyof paths> = paths[T][M]['responses']['200']['content']['application/json'];
 }
 
 export { };
